d248fa4766b631517644b946b056e7e7e7344263
Abhilekh Singh <144203765+bittusing@users.noreply.github.com>
2023-10-30 10:25:12 +0530

    ss

--- src/Loader.js
+++ src/Loader.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import { Audio } from 'react-loader-spinner';
-export default function Loader() {
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
   return (
-    <div>
-        <Audio
-  height="80"
-  width="80"
-  radius="9"
-  color="red"
-  ariaLabel="loading"
-  wrapperStyle
-  wrapperClass
-/>
+    <div className="loading">
+      <div></div>
     </div>
-  )
-}
+  );
+};
+
+export default Loader;
